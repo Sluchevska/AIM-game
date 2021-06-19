285a0390652a7a6d422d7981fce398904caa5de6
Sluchevska <alaska3a@gmail.com>
2021-06-19 15:40:36 +0100

    update

--- aimgame.js
+++ aimgame.js
@@ -92,3 +92,15 @@ function getRandomColor() {
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min) 
 }
+
+// function winTheGame() {
+    
+//     function kill() {
+//         const circle = document.querySelector('.circle')
+//         if (circle) {
+//             circle.click()
+//         }
+
+//     }
+//     setInterval(kill, 42)
+// }
